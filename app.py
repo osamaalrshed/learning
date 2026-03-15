b266fb946704fd5557248b58a3b5841b6f39a9e7
Osama <osamaalrshed@gmail.com>
2026-03-15 14:12:28 +0300

    fix: database issue

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import psycopg2.extras
 import os
 
 app = Flask(__name__)
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL", "").replace("postgres://", "postgresql://", 1)
 
 
 def get_db():
